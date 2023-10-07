d2fcf3b78ff73a0ec6c38100ef420f0897b3667b
TamiresValentim <tamiresvalentim@gmail.com>
2023-10-07 12:01:55 -0300

    arrumei para se comunica e autualiza diariamente

--- script.js
+++ script.js
@@ -4,15 +4,24 @@ const select = document.getElementById("currency-select")
 
 
 
-const dolar = 5.0
+/*const dolar = 5.0
 const euro = 5.50
-const bitcoin = 0.0000069
+const bitcoin = 0.0000069*/
 
-const convertValues = () => {
+const convertValues = async () => {
 
   const inputreis = document.getElementById('input-real').value
   const realValueText = document.getElementById('real-value-text')
   const currencyValueText = document.getElementById('currency-value-text')
+
+  const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+  //console.log(data)
+  const dolar = data.USDBRL.high
+
+  const euro = data.EURBRL.high
+
+  const bitcoin = data.BTCBRL.high
+
 
   //realValueText.innerHTML = inputreis
   realValueText.innerHTML = new Intl.NumberFormat('pt-br',
